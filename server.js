be5276d19b5be25e628ce7116d239b85b266af32
Jakhongir <jahongirnagmatov@gmail.com>
2021-11-02 22:18:51 +0500

    taking data from the form with body-parser

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
-const log = console.log;
 const path = require('path');
 const PORT = 8080;
 
-app.use(express.urlencoded({
-    extend: false
-}));
-app.use(express.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+app.post('/email', (req, res) => {
+    //Send an email here but currently dummy email
+    console.log("name :", req.body.name);
+    console.log("number: ", req.body.number);
+    
+});
+
 
 app.listen(PORT, function() {
-    log("Server is running on port 8080...")
+    console.log("Server is running on port 8080...")
 })
