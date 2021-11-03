ba56739c8f5fde2f4c6e7525a946293e0022451a
Jakhongir <jahongirnagmatov@gmail.com>
2021-11-03 15:02:13 +0500

    complete and debug with mailgun

--- server.js
+++ server.js
@@ -1,41 +1,51 @@
-const express = require('express');
+const express = require("express");
+const mailgun = require("mailgun-js");
 const bodyParser = require("body-parser");
-const sendMail = require('./mail');
+const dotenv = require("dotenv");
 
 const app = express();
-const path = require('path');
 const PORT = 8080;
+const path = require("path");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config();
 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
-})
+const API_KEY = process.env.API_KEY 
+const DOMAIN = process.env.DOMAIN 
+//Data parsing
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
 
-app.post('/email', (req, res) => {
-    // res.sendFile(path.join(__dirname + '/contact-us.html'));
-    //TODO
-    //send email here
-    const name = req.body.name
-    const number = req.body.number;
-    const Data = {
-        "Client's name": name,
-        "Client's number": number
-    }
-    console.log(Data);
-
-    sendMail(name, number, function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.status({ message: 'Email sent!!!' });
-        }
-    });
-    // res.json({ message: 'Message received!!!' })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-
-app.listen(PORT, function() {
-    console.log("Server is running on port 8080...")
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "success.html"))
 })
+
+app.post("/email", (req, res) => {
+  const clientName = req.body.fullName;
+  const clientNumber = req.body.phoneNumber
+
+  console.log(clientName, clientNumber)
+
+  const mg = mailgun({
+    apiKey: API_KEY,
+    domain: DOMAIN
+  });
+  const data = {
+    from: "Modern Academy <admin@modernacademy.uz>",
+    to: "jaykhansme@gmail.com",
+    subject: "Заявка через инстаграм modernacademy.uz",
+    text: `ФИО клиента: ${clientName}\nТелефон номер клиента: ${clientNumber}`
+  };
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+  res.redirect("/success")
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 8080...");
+});
